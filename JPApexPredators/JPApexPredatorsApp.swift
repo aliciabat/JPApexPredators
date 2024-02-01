9246918f6ad7fc7e6d2f654c9dc3c6f8086b9f14
m-batistella <aliciabatistella@hotmail.com>
2024-02-01 13:23:06 -0300

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by SEMEQ  on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
