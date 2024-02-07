f88f8273f045e8283900834f5e03c5cdd8753860
m-batistella <aliciabatistella@hotmail.com>
2024-02-07 10:39:46 -0300

    feat: predators map

---
+++ JPApexPredators/PredatorsMap.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorsMap.swift
+//  JPApexPredators
+//
+//  Created by SEMEQ  on 07/02/24.
+//
+
+import SwiftUI
+
+struct PredatorsMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorsMap()
+}
